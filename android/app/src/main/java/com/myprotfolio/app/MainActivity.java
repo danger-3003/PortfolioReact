bd4d5443f5fbc0bdae3d7dad3d1be83f930c7770
danger-3003 <naremsumanth@gmail.com>
2024-06-11 09:37:31 +0530

    added android folder

---
+++ android/app/src/main/java/com/myprotfolio/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.myprotfolio.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/Home/Footer.jsx
+++ src/Home/Footer.jsx
@@ -4,7 +4,7 @@ function Footer()
         <footer className="h-max pb-5 w-full bg-[#121212]">
             <div className="pt-4 flex justify-center items-center animate">
                 <div className="text-sm sm:text-base text-center">
-                    <p>Designed&nbsp;&&nbsp;Created&nbsp;by <a href="https://hima-varsha.vercel.app/" target="_blank" className="bg-gradient-to-br bg-clip-text text-transparent from-green-500 to-blue-500">Hima&nbsp;Varsha</a>&nbsp;,&nbsp;<a href="https://www.linkedin.com/in/sumanth-narem" target="_blank" className="bg-gradient-to-br bg-clip-text text-transparent from-green-500 to-blue-500">Sumanth&nbsp;Narem</a></p>
+                    <p>Designed&nbsp;by <a href="https://hima-varsha.vercel.app/" target="_blank" className="bg-gradient-to-br bg-clip-text text-transparent from-green-500 to-blue-500">Hima&nbsp;Varsha</a></p>
                 </div>
             </div>
         </footer>
